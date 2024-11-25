9f32ae0e19222cd05a045561321cbccd2924d01d
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2024-11-24 23:35:00 -0500

    temporary front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-export default function App() {
+import { Flex, Title, Text } from '@mantine/core';
+
+export default function App() { // read up on ways to define a react component
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Section 1 */}
+      <Flex
+        bg='#84A387'
+        h='100vh'
+        justify="center"
+        ta="center"
+      >
+        <Flex
+          gap="md"
+          direction="column"
+          c="#FFFAEB"
+          mt='20vh'
+        >
+          <Title size="5rem">Hello hello!<br/>I'm Sarah Wang</Title>
+          <Text size="1.5rem" w='50vw' lh="md">
+            I’m an aspiring software developer,
+            exploring the world of tech through the lens of social impact.
+          </Text>
+          <Text size="1.25rem" fw={300} mt="3rem">
+            More coming soon, I promise!
+          </Text>
+        </Flex>
+      </Flex>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createTheme, MantineProvider } from '@mantine/core';
 import App from './App'
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  fontFamily: 'Poppins, sans-serif',
+  headings: {
+    fontFamily: 'Gluten, sans-serif',
+    fontWeight: '600'
+  },
+  primaryColor: 'cyan',
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <MantineProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </MantineProvider>
 )
